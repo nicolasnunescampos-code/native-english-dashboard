b7a5658ad18020001cf4be2595f6fd9e6beb0282
Nicolas Campos <nicolasnunescampos@icloud.com>
2026-08-12 16:36:56 -0400

    Configure PWA to auto-update cache on publish

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,12 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      workbox: {
+        cleanupOutdatedCaches: true,
+        skipWaiting: true,
+        clientsClaim: true,
+      },
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
       manifest: {
         name: 'Native English Portal',
